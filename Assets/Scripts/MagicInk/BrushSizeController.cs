0be17a87f70f2b1aec5f4a34df2b1baa131fc68e
Benoit <benoit.morel@ameria.com>
2025-08-27 14:55:19 +0200

    Fix brush size controller

--- Assets/Scripts/MagicInk/BrushSizeController.cs
+++ Assets/Scripts/MagicInk/BrushSizeController.cs
@@ -18,7 +18,11 @@ public class BrushSizeController : MonoBehaviour
     public float maxBrushSize = 1.0f;
 
     private bool _isChangingBrushSize = false;
+    private float _startingSize = 0.0f;
     private float _startThumbX;
+
+    public float debugDelta = 0.0f;
+    private float debugX = 0.0f;
 
     void Update()
     {
@@ -29,7 +33,7 @@ public class BrushSizeController : MonoBehaviour
         }
 
         // Thumb vector (tip - base)
-        Vector3 thumbOrientation = magicHandGestures.magicHand.GetKeyPoint(3) - magicHandGestures.magicHand.GetKeyPoint(1);
+        Vector3 thumbOrientation = magicHandGestures.magicHand.GetKeyPoint(4) - magicHandGestures.magicHand.GetKeyPoint(1);
         thumbOrientation.Normalize();
         float dot = Vector3.Dot(thumbOrientation, thumbOrientationToStart);
         if (dot < thumbOrientationThreshold)
@@ -38,24 +42,28 @@ public class BrushSizeController : MonoBehaviour
             return;
         }
 
-        Vector3 thumbTip = magicHandGestures.magicHand.GetKeyPoint(3);
+        Vector3 thumbTip = magicHandGestures.magicHand.GetKeyPoint(4);
         Vector3 thumbBase = magicHandGestures.magicHand.GetKeyPoint(1); // base of thumb
-        float thumbX = thumbTip.x - thumbBase.x; // horizontal offset
+        float thumbX = thumbTip.x; // horizontal offset
 
         if (!_isChangingBrushSize)
         {
             _isChangingBrushSize = true;
             _startThumbX = thumbX; // store reference X
+            _startingSize = drawManager.brushSize;
         }
         else
         {
             float deltaX = thumbX - _startThumbX;
+            debugDelta = deltaX;
+            debugX = thumbX;
 
             // Horizontal movement drives scale
-            float scaleChange = Mathf.Exp(deltaX * brushSizeChangeSpeed * Time.deltaTime);
-            drawManager.brushSize *= scaleChange;
+            float scaleChange = Mathf.Exp(deltaX * brushSizeChangeSpeed);
+            drawManager.brushSize = _startingSize * scaleChange;
 
             drawManager.brushSize = Mathf.Clamp(drawManager.brushSize, minBrushSize, maxBrushSize);
+
         }
     }
 
