1ada23e56370ea93211d185c230187963bfb4d46
Benoit <benoit.morel@ameria.com>
2025-09-01 14:21:16 +0200

    3d map wip

--- Assets/Scripts/Demos/Magic3DMap/FPSMover.cs
+++ Assets/Scripts/Demos/Magic3DMap/FPSMover.cs
@@ -1,59 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class FPSMover : MonoBehaviour
+public class FreeFlyController : MonoBehaviour
 {
-    [SerializeField] CameraManager cameraManager;
-    [SerializeField] Transform sceneToMove;
-    [SerializeField] float moveSpeed = 1.0f;
-    [SerializeField] bool moveCamera = true;
+    public float moveSpeed = 5f;
+    public float mouseSensitivity = 2f;
+    public Transform sceneRoot; // assign your world root (all objects under here)
 
-    // Start is called before the first frame update
+    private float yaw = 0f;
+    private float pitch = 0f;
+    private bool moveCamera = true; // toggle with Tab
+
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        UpdateKeyboard();
-        UpdateCameraFromTarget();
+        if (Input.GetKeyDown(KeyCode.Tab))
+            moveCamera = !moveCamera;
 
-       
+        HandleMouseLook();
+        HandleMovement();
     }
 
-    private void UpdateKeyboard()
+    void HandleMouseLook()
     {
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position += Vector3.forward * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position += Vector3.back * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            transform.position += Vector3.left * Time.deltaTime * moveSpeed;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.position += Vector3.right * Time.deltaTime * moveSpeed;
-        }
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        yaw += mouseX;
+        pitch -= mouseY;
+        pitch = Mathf.Clamp(pitch, -89f, 89f); // prevent flipping
     }
 
-    private void UpdateCameraFromTarget()
+    void HandleMovement()
     {
+        float h = Input.GetAxis("Horizontal"); // A/D
+        float v = Input.GetAxis("Vertical");   // W/S
+
+        // Full rotation (yaw + pitch)
+        Quaternion rotation = Quaternion.Euler(pitch, yaw, 0f);
+
+        // Forward is camera's forward, Right is camera's right
+        Vector3 move = (rotation * new Vector3(h, 0, v)) * moveSpeed * Time.deltaTime;
+
         if (moveCamera)
-        {
-            cameraManager.mainCamera.transform.position = transform.position;
-            cameraManager.mainCamera.transform.rotation = transform.rotation;   
-        } else
-        {
-            sceneToMove.position = -transform.position;
-            sceneToMove.rotation = Quaternion.Inverse(transform.rotation);
-        }
+            transform.position += move;
+        else
+            sceneRoot.position -= move;
+    }
+
+    void LateUpdate()
+    {
+        // Apply pitch + yaw to the actual camera
+        Camera.main.transform.SetPositionAndRotation(
+            transform.position,
+            Quaternion.Euler(pitch, yaw, 0f)
+        );
     }
 }
