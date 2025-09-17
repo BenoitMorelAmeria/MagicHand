6eac79365b108aff08aa1491c8c8ee370ecf1a23
Benoit <benoit.morel@ameria.com>
2025-09-17 08:55:07 +0200

    improve visual feedback

--- Assets/Demos/Magic3DMap/Scripts/HandOrientationController.cs
+++ Assets/Demos/Magic3DMap/Scripts/HandOrientationController.cs
@@ -25,9 +25,11 @@ public class HandOrientationController : MonoBehaviour
     [SerializeField] private float timeBeforeStopInteraction = 0.5f;
 
     [SerializeField] private GameObject visualFeedbackPrefab;
+    [SerializeField] private GameObject visualFeedbackInsidePrefab;
     [SerializeField] private GameObject visualFeedbackNeutralPrefab;
 
     private GameObject visualFeedback;
+    private GameObject visualInsideFeedback;
     private GameObject visualFeedbackNeutral;
 
     private bool _isInteracting = false;
@@ -47,7 +49,9 @@ public class HandOrientationController : MonoBehaviour
     {
         handNeutral = Quaternion.Euler(handNeutralEuler);
         visualFeedback = Instantiate(visualFeedbackPrefab, transform.parent);
+        visualInsideFeedback = Instantiate(visualFeedbackInsidePrefab, visualFeedback.transform);
         visualFeedbackNeutral = Instantiate(visualFeedbackNeutralPrefab, transform.parent);
+
     }
 
     private void OnValidate()
@@ -128,20 +132,24 @@ public class HandOrientationController : MonoBehaviour
         {
             HandleHandPoseFreeFlying();
         }
+    }
 
+    private Vector3 GetRelativePosition()
+    {
 
-
-
+        Vector3 handPosePos = magicHandGestures.magicHand.Data.GetKeypointScreenSpace(9);
+        Vector3 relativePos = handPosePos - neutralPosition;
+        relativePos.x = ApplyDeadZone(relativePos.x, handDeadZone.x);
+        relativePos.y = ApplyDeadZone(relativePos.y, handDeadZone.y);
+        relativePos.z = ApplyDeadZone(relativePos.z, handDeadZone.z);
+        return relativePos;
     }
 
     private void HandleHandPoseFreeFlying()
     {
         // translation using the relative position
         Vector3 handPosePos = magicHandGestures.magicHand.Data.GetKeypointScreenSpace(9);
-        Vector3 relativePos = handPosePos - neutralPosition;
-        relativePos.x = ApplyDeadZone(relativePos.x, handDeadZone.x);
-        relativePos.y = ApplyDeadZone(relativePos.y, handDeadZone.y);
-        relativePos.z = ApplyDeadZone(relativePos.z, handDeadZone.z);
+        Vector3 relativePos = GetRelativePosition();
         Vector3 withRotationComponent = new Vector3(relativePos.x, 0, relativePos.z);
         transform.position += transform.rotation * Vector3.Scale(withRotationComponent, handPoseTranslationSpeed) * Time.deltaTime;
         Vector3 withoutRotationComponent = new Vector3(0, relativePos.y, 0);
@@ -212,6 +220,12 @@ public class HandOrientationController : MonoBehaviour
         visualFeedback.SetActive(active);
         visualFeedback.transform.position = magicHandGestures.magicHand.Data.GetKeypoint(9);
         visualFeedback.transform.rotation = ComputeRotation();
+
+        visualInsideFeedback.SetActive(active);
+        visualInsideFeedback.transform.localScale = new Vector3(0.99f, 0.99f, 0.99f);
+        visualInsideFeedback.transform.localPosition = new Vector3(0,
+            0,
+            -GetRelativePosition().z);
 
         visualFeedbackNeutral.SetActive(active);
         visualFeedbackNeutral.transform.position = neutralPosition;
